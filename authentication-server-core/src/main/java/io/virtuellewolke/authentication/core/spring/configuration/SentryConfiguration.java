f3aa6ba2f9cc007ccc2789deebf91034a23629f0
Mirco Tries <mircotries@gmx.com>
2020-07-08 00:38:18 +0200

    Added SentryConfiguration (was accidentally removed)

---
+++ authentication-server-core/src/main/java/io/virtuellewolke/authentication/core/spring/configuration/SentryConfiguration.java
@@ -0,0 +1,33 @@
+package io.virtuellewolke.authentication.core.spring.configuration;
+
+import io.sentry.spring.SentryExceptionResolver;
+import io.virtuellewolke.authentication.core.exceptions.AccessDeniedException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+@Configuration
+@ConditionalOnProperty(prefix = "sentry", name = "dsn")
+public class SentryConfiguration {
+    public SentryConfiguration() {
+        log.info("Sentry configured and enabled.");
+    }
+
+    @Bean
+    public HandlerExceptionResolver sentry() {
+        return new SentryExceptionResolver() {
+            @Override
+            public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+                if (ex instanceof AccessDeniedException) return null;
+                return super.resolveException(request, response, handler, ex);
+            }
+        };
+    }
+}
